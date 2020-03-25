6c03594e5095c3d7445d68a3b980f1534c32a1ab
vetordev <you@example.com>
2020-03-25 13:59:40 -0300

    backend finished;

---
+++ README.md
@@ -0,0 +1,3 @@
+# Semana Omnistack 11 - Be The Hero
+
+**curso cedido pela rocketseat**
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'
+
+function App() {
+  return (
+    <Header>
+      Semana Oministack
+    </Header>
+      
+  );
+}
+
+export default App;
